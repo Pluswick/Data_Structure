53b6e7d3753724be0637e602cd6a509a08f255d4
Pluswick <tjdgks0814@gmail.com>
2025-04-09 16:18:29 +0900

    upload assignment for week05

---
+++ KSH/week05/stack_01/stack_01/ex5_01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"stackS.h"
+
+int main(void) {
+	element item;
+	printStack();
+	push(1); printStack();
+	push(2); printStack();
+	push(3); printStack();
+
+	item = peek(); printStack();
+	printf("peek => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	return 0;
+}
